3054df3db04157cfd165a52be80d2d2d92d2437f
darryn.campbell <darryn.campbell@pubnub.com>
2023-05-31 10:46:47 +0100

    Applying Access Manager, moving moderation to function, updating SDK version

--- js/pubnub_setup.js
+++ js/pubnub_setup.js
@@ -4,7 +4,7 @@
  */
 
 //  Ideally the PubNub object will be created with an ID based on the URL query string but if that is not specified, generate a new URL and store it in session storage.
-function createPubNubObject (presetUUID) {
+async function createPubNubObject (presetUUID) {
   var UUID = presetUUID // Allows you to force a uuid
   let savedUUID = null
   if (!UUID) {
@@ -29,6 +29,19 @@ function createPubNubObject (presetUUID) {
     subscribeKey: subscribe_key,
     uuid: UUID
   })
+  var accessManagerToken = await requestAccessManagerToken(UUID);
+  if (accessManagerToken == null)
+  {
+    console.log('Error retrieving access manager token')
+  }
+  else
+  {
+    pubnub.setToken(accessManagerToken)
+    //  The server that provides the token for this app is configured to grant a time to live (TTL)
+    //  of 360 minutes (i.e. 6 hours).  IN PRODUCTION, for security reasons, you should set a value 
+    //  between 10 and 60 minutes and refresh the token before it expires.
+    //  For simplicity, this app does not refresh the token
+  }
   return pubnub
 }
 
@@ -45,4 +58,25 @@ function makeid (length) {
 function testPubNubKeys () {
   if (publish_key === '' || subscribe_key === '') return false
   else return true
+}
+
+async function requestAccessManagerToken (userId) {
+  try {
+    const TOKEN_SERVER = 'https://devrel-demos-access-manager.netlify.app/.netlify/functions/api/deliverydemo'
+    const response = await fetch(`${TOKEN_SERVER}/grant`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ UUID: userId })
+    })
+
+    const token = (await response.json()).body.token
+    //console.log('created token: ' + token)
+
+    return token
+  } catch (e) {
+    console.log('failed to create token ' + e)
+    return null
+  }
 }
